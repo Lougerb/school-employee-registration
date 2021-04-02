86c2f72eeba34b6c0d0419a7bbf4a52d0207fb6a
Louein Gerald Baling <lgbalingbiz@gmail.com>
2021-04-02 20:36:13 +0800

    Submit button is working

--- reg-script.js
+++ reg-script.js
@@ -8,10 +8,13 @@ const currentYear = currentDate.getFullYear();
 
 let batchNum = 0;
 let empNum = 0;
-const regSubBtn = document.querySelector(".subButton");
 
+const regSubBtn = document.getElementById("submitButton");
+
+//addEventListener must listen to the Form INSTEAD of the Submit Button of the form
+const regForm = document.getElementById("regForm");
 //Submit Button
-regSubBtn.addEventListener("submit", function (e) {
+regForm.addEventListener("submit", function (e) {
   e.preventDefault();
   const getLName = document.querySelector("#lName");
   const getFName = document.querySelector("#fName");
@@ -19,19 +22,19 @@ regSubBtn.addEventListener("submit", function (e) {
   const getSName = document.querySelector("#sName");
   const getEmpID = document.querySelector("#eIDNumber");
   const getPosition = document.querySelector("#jPosition");
-  // const getEmail = document.querySelector("#eMail");
+
+  const getEmail = document.querySelector("#eMail");
   // const getDOB = document.querySelector("#bDay");
-  // const getGender = document.querySelector("#sGender");
-  // const getStatus = document.querySelector("#sStatus");
-  // const getLTeach = document.querySelector("#lTeach");
-  // const getSubject = document.querySelector("#posSubject");
+  const getGender = document.querySelector("#sGender");
+  const getStatus = document.querySelector("#sStatus");
+  const getLTeach = document.querySelector("#lTeach");
+  const getSubject = document.querySelector("#posSubject");
 
   const parentTable = document.querySelector("#empInfo");
   const newEmpRow = document.createElement("tr");
   const newEmpID = document.createElement("th");
   const newPosition = document.createElement("th");
   const newFullName = document.createElement("th");
-  //   const newEmail = document.createElement("th"); //Optional
   const newActionBtn = document.createElement("th"); //Must be Button
 
   parentTable.appendChild(newEmpRow);
@@ -48,20 +51,21 @@ regSubBtn.addEventListener("submit", function (e) {
   newActionBtn.textContent = "*";
 
   //clearing after submitting
-  //   getLName.value = "";
-  //   getFName.value = "";
-  //   getMName.value = "";
-  //   getSName.value = "";
-  //   getEmpID.value = "";
-  //   getPosition.value = "";
-  //   getEmail.value = "";
-  //   getDOB.value = "";
-  //   getGender.value = "";
-  //   getStatus.value = "";
-  //   getLTeach.value = "";
-  //   getSubject.value = "";
+  getLName.value = "";
+  getFName.value = "";
+  getMName.value = "";
+  getSName.value = "";
+  getEmpID.value = "";
+  getPosition.value = "";
+  getEmail.value = "";
+  // getDOB.value = "";
+  getGender.value = "";
+  getStatus.value = "";
+  getLTeach.value = "";
+  getSubject.value = "";
 
   //   console.log(getLName.value[0]);
+  console.log("working!");
 });
 
 //Month's dates
@@ -95,7 +99,7 @@ inputMonth.addEventListener("click", function () {
 //Job position
 // const
 
-//Object
+//Object;
 const regEmployees = {
   employeeID: {
     202100101: {
